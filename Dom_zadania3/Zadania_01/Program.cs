9a94fdc2c0bfb9b03c22b21e17d7a9be9418e825
Daulet <moni_i@mail.ru>
2022-04-28 07:29:49 +0600

    Задача 1

---
+++ Dom_zadania3/Zadania_01/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+
+bool ProvPalin(string s, int i, int j)
+{
+    bool result = true;
+    if (j >= 0)
+    {
+        result = (s[i] == s[j]) && ProvPalin(s, ++i, --j);
+    }
+    return result;
+}
+int i = 23432;
+Console.WriteLine(ProvPalin(i.ToString(), 0, i.ToString().Length - 1));
+
