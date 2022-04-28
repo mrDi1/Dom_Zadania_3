3ebedc2d7ae38ec00f693a522af02d3a7360f662
Daulet <moni_i@mail.ru>
2022-04-28 19:19:21 +0600

    Блок схему добавил

--- Dom_zadania3/Zadania_02/Program.cs
+++ Dom_zadania3/Zadania_02/Program.cs
@@ -3,7 +3,6 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-
 Double x1 = 3;
 Double x2 = 2;
 Double y1 = 6;
@@ -12,13 +11,12 @@ Double z1 = 8;
 Double z2 = -7;
 Double dis;
 
-// distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-// distance = Math.Round(distance,2);
-// Console.WriteLine($"Расстояние между двумя точками на плоскости равно {distance}");
-
+// dis = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+// dis = Math.Round(dis,2);
+// Console.WriteLine($"Расстояние между двумя точками на плоскости равно {dis}");
 
 dis = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
-dis = Math.Round(dis,2);
+dis = Math.Round(dis,2);                                                                    // округлили остаток
 Console.WriteLine($"Расстояние между двумя точками в пространстве равно {dis}");
 
 // Методы мне совсем не даются (((
